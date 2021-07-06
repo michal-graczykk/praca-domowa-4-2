a3582faa3f65408ae73da74b33208613a9dff74b
michal.graczykk <michal.graczykk@gmail.com>
2021-07-06 18:21:08 +0200

    Praca domowa 2 tydzień 4

---
+++ PracaDomowa4_2/GetString.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracaDomowa4_2
+{
+    class GetString
+    {
+        public string FizzBuzz(int argument)
+        {
+            if ((argument % 3 == 0) && (argument % 5 == 0))
+                return "FizzBuzz";
+
+            else if (argument % 3 == 0)
+                return "Fizz";
+
+            else if (argument % 5 == 0)
+                return "Buzz";
+            //else
+            //    return " ";
+            else
+                return argument.ToString();
+        }
+    }
+}
---
+++ PracaDomowa4_2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracaDomowa4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Podaj proszę liczbę: ");
+                
+                var fizzBuzz = new GetString();
+
+                while (true)
+                {
+                    int number = GetNumber();
+
+                    Console.WriteLine(fizzBuzz.FizzBuzz(number));
+                }
+                
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+
+
+
+        }
+        private static int GetNumber()
+        {
+            if (!int.TryParse(Console.ReadLine(), out int number))
+            {
+                throw new Exception("Podana wartość jest nieprawidłowa!");
+            }
+            return number;
+        }
+
+        
+    }
+}
